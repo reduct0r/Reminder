f6866bcb45e1d7b91581ffc7b599bd7b3baedbce
Exist <exist@Exists-MacBook-Pro.local>
2024-04-19 22:38:42 +0300

    Updated CMakeLists and database class

--- Reminder/Database/Database.cpp
+++ Reminder/Database/Database.cpp
@@ -1,5 +1,2 @@
-//
-// Created by Exist on 19.04.2024.
-//
-
 #include "Database.h"
+
--- Reminder/Database/Database.h
+++ Reminder/Database/Database.h
@@ -1,12 +1,14 @@
-//
-// Created by Exist on 19.04.2024.
-//
-
 #ifndef REMINDER_REMINDER_DATABASE_DATABASE_H_
 #define REMINDER_REMINDER_DATABASE_DATABASE_H_
 
+#include <string>
+#include "pqxx/pqxx"
+
 class Database {
+ private:
+  static std::string connectionString;
+ public:
 
 };
 
-#endif //REMINDER_REMINDER_DATABASE_DATABASE_H_
+#endif
