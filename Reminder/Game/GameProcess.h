a5affb68a47d72fca005387327f48d7d68b5ab6c
exist <gmi23u835@student.bmstu.ru>
2024-05-19 13:04:48 +0300

    Rollback-v2

--- Reminder/Game/GameProcess.h
+++ Reminder/Game/GameProcess.h
@@ -8,45 +8,6 @@
 #include <iostream>
 #include "json/json.h"
 
-//
-//#pragma once
-//#include "../Sections/PreesetsMenu/PresetsMenu.h"
-//
-//class Game {
-//private:
-//    Settings gfxSettings;
-//
-//    sf::RenderWindow* window;
-//    sf::Event sfEvent;
-//    sf::Clock dtClock;
-//    float dt;
-//
-//    std::stack<State*> states;
-//
-//    void InitVars();
-//    void InitWindow();
-//    void InitStates();
-//    void InitSettings();
-//
-//public:
-//    // Core
-//    Game();
-//    virtual ~Game();
-//    void Run();
-//
-//    // Update
-//    void Update();
-//    void UpdateEvents();
-//    void UpdateDT();
-//
-//    //Render
-//    void Render();
-//
-//    // Functions
-//    void EndApplication();
-//
-//};
-
 namespace Reminder {
 class Card {
  public:
